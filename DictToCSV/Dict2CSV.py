3e27c292fe796e17ad52592fd4b9bad3c6d63277
AndreyShapovalovVN <a147@ukr.net>
2020-02-14 11:22:03 +0200

    BagFix

--- DictToCSV/Dict2CSV.py
+++ DictToCSV/Dict2CSV.py
@@ -2,58 +2,29 @@ import csv
 import os
 from uuid import uuid4
 
-DATA_REQUEST = {}
-
-
-def xParce(data, parent, id=uuid4().hex, p_id=None):
-    response = {'id': id, 'p_id': p_id}
-    for item in data:
-        if not DATA_REQUEST.get(parent):
-            DATA_REQUEST[parent] = []
-
-        if isinstance(data[item], list):
-            for list_item in data[item]:
-                xParce(list_item, item, id=uuid4().hex, p_id=id)
-
-        elif isinstance(data[item], type(None)):
-            response[item] = None
-
-        elif isinstance(data[item], str):
-            response[item] = data[item]
-
-        elif isinstance(data[item], int):
-            response[item] = data[item]
-
-        elif isinstance(data[item], float):
-            response[item] = data[item]
-
-        else:
-            xParce(data[item], item, id=id, p_id=p_id)
-
-    if len(response) > 2:
-        DATA_REQUEST[parent].append(response)
-    else:
-        del DATA_REQUEST[parent]
-
 
 class to_csv:
     _delimiter = ';'
     _codepage = 'utf8'
 
     def __init__(self, data=None):
-        self.data = data or DATA_REQUEST
+        self.data = data or {}
 
     @property
     def header(self):
         data = self.data.get(self.file)
         head = {}
-        for i in range(len(self.data)):
+        for i in range(len(data)):
             head = head | data[i].keys()
         return list(head)
+
+    def clear(self):
+        self.data = {}
 
     def save_all(self, path='./'):
         for k in self.data.keys():
             self.save_one(k, path=path)
+        self.clear()
 
     def save_one(self, file, path='./', header=None):
         if self.data.get(file):
@@ -67,3 +38,33 @@ class to_csv:
             )
             out_csv.writeheader()
             out_csv.writerows(self.data[file])
+
+    def xParce(self, data, parent, id=uuid4().hex, p_id=None):
+        response = {'id': id, 'p_id': p_id}
+        for item in data:
+            if not self.data.get(parent):
+                self.data[parent] = []
+
+            if isinstance(data[item], list):
+                for list_item in data[item]:
+                    self.xParce(list_item, item, id=uuid4().hex, p_id=id)
+
+            elif isinstance(data[item], type(None)):
+                response[item] = None
+
+            elif isinstance(data[item], str):
+                response[item] = data[item]
+
+            elif isinstance(data[item], int):
+                response[item] = data[item]
+
+            elif isinstance(data[item], float):
+                response[item] = data[item]
+
+            else:
+                self.xParce(data[item], item, id=id, p_id=p_id)
+
+        if len(response) > 2:
+            self.data[parent].append(response)
+        else:
+            del self.data[parent]
